76341638c89ef138b579cc2f6d759c8c37c434d4
PPG <g277321@gmail.com>
2018-05-29 23:06:31 +0200

    with js standard style and new readme  demo

--- sec_wallet_lib.js
+++ sec_wallet_lib.js
@@ -3,127 +3,127 @@ const crypto = require('crypto')
 const EC = require('elliptic').ec
 const RIPEMD160 = require('ripemd160')
 const bs58 = require('bs58')
-//const buffer = require('buffer')
+/**
+ * not sure whether useful
+ * const buffer = require('buffer')
+ */
+
 const ec = new EC('secp256k1')
 
+/**
+ * using EC crypto generate sec private key public key and wif address
+ * generate sec wallet
+ */
+
 class secWallet {
+  constructor () {
+    this.privKey = ''
+    this.publicKey = ''
+    this.secWifAddress = ''
+    this.secAddress = ''
+    this.generatePrivateKey()
+  }
 
-    constructor() {
-        this.privKey = ''
-        this.publicKey = ''
-        this.secWifAddress = ''
-        this.secAddress = ''
-        this.generatePrivateKey()
-    }
+  /**
+   * A small function created as there is a lot of sha256 hashing.
+   * @param  {Buffer} data -creat sha256 hash buffer
+   */
 
-    /**
-     * A small function created as there is a lot of sha256 hashing. 
-     */
-    hasha256(data) {
-        return crypto.createHash('sha256').update(data).digest()
-    }
+  hasha256 (data) {
+    return crypto.createHash('sha256').update(data).digest()
+  }
+  /**
+   * 0x00 P2PKH Mainnet, 0x6f P2PKH Testnet
+   * 0x80 Mainnet, 0xEF Testnet （or Test Network: 0x6f and Namecoin Net:0x34）
+   * generate private key through sha256 random values. and translate to hex
+   * get usedful private key. It will be used for secp256k1
+   * generate check code. two times SHA256 at privatKey.
+   * base58(privat key + the version number + check code).
+   * it is used as WIF(Wallet import Format) privatKey
+   */
+  generatePrivateKey () {
+    let addrVer = Buffer.alloc(1, 0x00)
+    let wifByte = Buffer.alloc(1, 0x80)
 
-    generatePrivateKey() {
-        /** 
-         * 0x00 P2PKH Mainnet, 0x6f P2PKH Testnet
-         * 0x80 Mainnet, 0xEF Testnet （or Test Network: 0x6f and Namecoin Net:0x34）
-         */
-        let addrVer = Buffer.alloc(1, 0x00)
-        let wifByte = Buffer.alloc(1, 0x80)
+    let key = ec.genKeyPair()
+    this.privKey = key.getPrivate().toString('hex')
 
-        /**
-         * generate private key through sha256 random values. and translate to hex
-         */
-        let key = ec.genKeyPair()
-        this.privKey = key.getPrivate().toString('hex')
+    let bufPrivKey = Buffer.from(this.privKey, 'hex')
+    let wifBufPriv = Buffer.concat([wifByte, bufPrivKey], wifByte.length + bufPrivKey.length)
 
-        /**
-         * get usedful private key. It will be used for secp256k1
-         */
-        let bufPrivKey = Buffer.from(this.privKey, 'hex')
-        let wifBufPriv = Buffer.concat([wifByte, bufPrivKey], wifByte.length + bufPrivKey.length)
+    let wifHashFirst = this.hasha256(wifBufPriv)
+    let wifHashSecond = this.hasha256(wifHashFirst)
 
-        /**
-         * generate check code. two times SHA256 at privatKey.
-         */
-        let wifHashFirst = this.hasha256(wifBufPriv)
-        let wifHashSecond = this.hasha256(wifHashFirst)
+    let wifHashSig = wifHashSecond.slice(0, 4)
+    let wifBuf = Buffer.concat([wifBufPriv, wifHashSig], wifBufPriv.length + wifHashSig.length)
 
-        let wifHashSig = wifHashSecond.slice(0, 4)
-        let wifBuf = Buffer.concat([wifBufPriv, wifHashSig], wifBufPriv.length + wifHashSig.length)
+    let wifFinal = bs58.encode(wifBuf)
+    this.secWifAddress = wifFinal.toString('hex')
+    this.generatePublicKey(key, addrVer)
+  }
 
-        /**
-         * base58(privat key + the version number + check code).
-         * it is used as WIF(Wallet import Format) privatKey
-         */
-        let wifFinal = bs58.encode(wifBuf)
-        this.secWifAddress = wifFinal.toString('hex')
-        this.generatePublicKey(key, addrVer)
-    }
+  /**
+   * generate public key
+   * @param  {Buffer} key
+   * @param  {Buffer} addrVer -input addVer from generatePrivateKey()
+   * set elliptic point and x,y axis
+   * not sure whether useful
+   * let x = pubPoint.getX()
+   * let y = pubPoint.getY()
+   * use secp256k1. generate public key
+   * structe public key: 1(network ID) + 32bytes(from x axis) + 32bytes(from y axis)
+   * ripemd160(sha256(public key))
+   */
+  generatePublicKey (key, addrVer) {
+    let pubPoint = key.getPublic()
 
-    generatePublicKey(key, addrVer) {
-        /**
-         * set elliptic point and x,y axis
-         */
-        let pubPoint = key.getPublic()
-        //let x = pubPoint.getX()
-        //let y = pubPoint.getY()
+    this.publicKey = pubPoint.encode('hex')
+    this.generateAddress(this.publicKey, addrVer)
+  }
 
-        /**
-         * use secp256k1. generate public key
-         * structe public key: 1(network ID) + 32bytes(from x axis) + 32bytes(from y axis)
-         * ripemd160(sha256(public key))
-         */
-        this.publicKey = pubPoint.encode('hex')
-        this.generateAddress(this.publicKey, addrVer)
-    }
+  /**
+   * double sha256 generate hashExtRipe2. sha256(sha256(version number + hashBuffer)).
+   * the first 4 bytes of hashExtRipe2 are used as a checksum and placed at the end of
+   * the 21 byte array. structe secBinary: 1(network ID) + concatHash + 4 byte(checksum)
+   * @param  {Buffer} publicKey -input public key from generatePublicKey()
+   * @param  {Buffer} addrVer -input addVer from generatePrivateKey()
+   * generate WIF private key and translate to hex
+   * generate SEC Address and translate to hex
+   */
+  generateAddress (publicKey, addrVer) {
+    let publicKeyInitialHash = this.hasha256(Buffer.from(publicKey, 'hex'))
+    let publicKeyRIPEHash = new RIPEMD160().update(Buffer.from(publicKeyInitialHash, 'hex')).digest('hex')
 
-    generateAddress(publicKey, addrVer) {
-        let publicKeyInitialHash = this.hasha256(Buffer.from(publicKey, 'hex'))
-        let publicKeyRIPEHash = new RIPEMD160().update(Buffer.from(publicKeyInitialHash, 'hex')).digest('hex')
+    let hashBuffer = Buffer.from(publicKeyRIPEHash, 'hex')
+    let concatHash = Buffer.concat([addrVer, hashBuffer], addrVer.length + hashBuffer.length)
 
-        let hashBuffer = Buffer.from(publicKeyRIPEHash, 'hex')
-        let concatHash = Buffer.concat([addrVer, hashBuffer], addrVer.length + hashBuffer.length)
+    let hashExtRipe = this.hasha256(concatHash)
+    let hashExtRipe2 = this.hasha256(hashExtRipe)
+    let hashSig = hashExtRipe2.slice(0, 4)
+    let secBinaryStr = Buffer.concat([concatHash, hashSig], concatHash.length + hashSig.length)
 
-        /**
-         * double sha256 generate hashExtRipe2. sha256(sha256(version number + hashBuffer)).
-         * the first 4 bytes of hashExtRipe2 are used as a checksum and placed at the end of 
-         * the 21 byte array. structe secBinary: 1(network ID) + concatHash + 4 byte(checksum)
-         */
-        let hashExtRipe = this.hasha256(concatHash)
-        let hashExtRipe2 = this.hasha256(hashExtRipe)
-        let hashSig = hashExtRipe2.slice(0, 4)
-        let secBinaryStr = Buffer.concat([concatHash, hashSig], concatHash.length + hashSig.length)
+    this.secAddress = bs58.encode(Buffer.from(secBinaryStr))
+  }
 
+  /**
+   * return four private key, wif private key, public key
+   * and sec address
+   */
+  getPrivateKey () {
+    return this.privKey
+  }
 
-        /**
-         * generate WIF private key and translate to hex
-         * generate SEC Address and translate to hex
-         */
+  getsecWifFinal () {
+    return this.secWifAddress
+  }
 
-        this.secAddress = bs58.encode(Buffer.from(secBinaryStr))
-    }
+  getPublicKey () {
+    return this.publicKey
+  }
 
-    /**
-     * return four private key, wif private key,public key
-     * and sec address
-     */
-    getPrivateKey() {
-        return this.privKey
-    }
-
-    getsecWifFinal() {
-        return this.secWifAddress
-    }
-
-    getPublicKey() {
-        return this.publicKey
-    }
-
-    getAddress() {
-        return this.secAddress
-    }
-
+  getAddress () {
+    return this.secAddress
+  }
 }
 
 module.exports = secWallet
--- test.md
+++ test.md
@@ -1,11 +1,7 @@
 <a name="secWallet"></a>
-[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard) 
-
-[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)] 
 
 ## secWallet
-using EC crypto generate sec private key public key and wif address
-generate sec wallet
+using EC crypto generate sec private key public key and wif addressgenerate sec wallet
 
 **Kind**: global class  
 
@@ -30,13 +26,7 @@ A small function created as there is a lot of sha256 hashing.
 <a name="secWallet+generatePrivateKey"></a>
 
 ### secWallet.generatePrivateKey()
-0x00 P2PKH Mainnet, 0x6f P2PKH Testnet
-0x80 Mainnet, 0xEF Testnet （or Test Network: 0x6f and Namecoin Net:0x34）
-generate private key through sha256 random values. and translate to hex
-get usedful private key. It will be used for secp256k1
-generate check code. two times SHA256 at privatKey.
-base58(privat key + the version number + check code).
-it is used as WIF(Wallet import Format) privatKey
+0x00 P2PKH Mainnet, 0x6f P2PKH Testnet0x80 Mainnet, 0xEF Testnet （or Test Network: 0x6f and Namecoin Net:0x34）generate private key through sha256 random values. and translate to hexget usedful private key. It will be used for secp256k1generate check code. two times SHA256 at privatKey.base58(privat key + the version number + check code).it is used as WIF(Wallet import Format) privatKey
 
 **Kind**: instance method of [<code>secWallet</code>](#secWallet)  
 <a name="secWallet+generatePublicKey"></a>
@@ -54,9 +44,7 @@ generate public key
 <a name="secWallet+generateAddress"></a>
 
 ### secWallet.generateAddress(publicKey, addrVer)
-double sha256 generate hashExtRipe2. sha256(sha256(version number + hashBuffer)).
-the first 4 bytes of hashExtRipe2 are used as a checksum and placed at the end of
-the 21 byte array. structe secBinary: 1(network ID) + concatHash + 4 byte(checksum)
+double sha256 generate hashExtRipe2. sha256(sha256(version number + hashBuffer)).the first 4 bytes of hashExtRipe2 are used as a checksum and placed at the end ofthe 21 byte array. structe secBinary: 1(network ID) + concatHash + 4 byte(checksum)
 
 **Kind**: instance method of [<code>secWallet</code>](#secWallet)  
 
@@ -68,18 +56,6 @@ the 21 byte array. structe secBinary: 1(network ID) + concatHash + 4 byte(checks
 <a name="secWallet+getPrivateKey"></a>
 
 ### secWallet.getPrivateKey()
-return four private key, wif private key, public key
-and sec address
+return four private key, wif private key, public keyand sec address
 
 **Kind**: instance method of [<code>secWallet</code>](#secWallet)  
-
-## SEC 地址
------------------------------------------------
-得到SEC公钥以后，需要通过公钥得到地址。由于椭圆曲线算法生成的公钥信息比较长，压缩格式的有33字节，非压缩的则有65字节。
-
-地址是为了减少接收方所需标识的字节数。SEC地址（secAddress）的生成步骤如下：
-
-1. 将公钥通过SHA256哈希算法处理得到32字节的哈希值，
-2. 后对得到的哈希值通过RIPEMD-160算法来得到20字节的哈希值 —— Hash160  即ripemd160（sha256（publicKey））
-3. 把版本号[2]+Hash160组成的21字节数组进行双次SHA256哈希运算，得到的哈希值的头4个字节作为校验和，放置21字节数组的末尾。
-4. 对组成25位数组进行Base58编码，就得到地址。
